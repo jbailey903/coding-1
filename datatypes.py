5b7cf918bc4d5b8ee0b330436959d794a6a1eb79
jbailey903 <jbailey903@boyslatin.net>
2026-09-01 13:36:47 +0000

    datatype

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+# string- characters that are wrapped within quotes
+# the hashtag is a marker that makes anything written
+# behind it invisible to the computer
+# intergers- datatypes that are just whole numbers, or numbers
+# without a decemil point
+# floats- A number with a decimal
+# boolean a data type that can only be either true or false
+# we di not use quotes
