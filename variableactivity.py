1531c5cb387dc9758ea9c9df69bf35ab51c2fa29
jbailey903 <jbailey903@boyslatin.net>
2026-09-03 13:44:32 +0000

    classwork

---
+++ variableactivity.py
@@ -0,0 +1,26 @@
+# variable- a container that stores data 
+# name your container almost everything you want, so long
+# as it is descriptive
+
+name=" Jaquan"
+yearsofexp=1
+graduate=false
+
+# when  we want to create a variable, we make up a word,
+# use the equal sign and then assign it a data type
+# whenever the computer sees our vairable word it will
+# always use the data inside
+
+# variable naming rules
+# variable must start with a letter
+# variable can be a mix of letters
+# a variable cannot be a python keyword
+
+
+applecount= 10
+mathstudents=
+insuranceammonut=
+gymmembership
+priceofsprite=3.50
+priceofshrilytemple=2.45
+priceofpepsi=3.00
