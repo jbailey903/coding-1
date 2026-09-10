012b0d2677bfd19d8f99cb80ce35b5439ecafb6f
jbailey903 <jbailey903@boyslatin.net>
2026-09-10 13:39:02 +0000

    notes

--- unit 1/operators.py
+++ unit 1/operators.py
@@ -1,30 +1,3 @@
-# assigment operators- exclusively used to
-# assign values to variables
-# think key/ value pairings
-
-# we use a single equal sign to repersent the assingment
-# operator
-name='jaquan'
-grade=12th
-school= true
-
-# arithmetic operators-used on numericalo
-# data typesn to perform calculations
-# intergers ( whole numbers) and floats ( decimal numbers)
-
-# print is a function that lets us show code
-# in the terminal
-print(3 +3) # addition operator
-print(3-3) # subtraction operator
-print(12/3) # division operator
-print(3 * 4) # multiplication operator
-
 # comparison operators- set of symbols used
 # to asses if data is the same or different and
 # how they differ
-
-print(10 > 1) # greater than operator
-print(10 <2) # less than operator 
-
-# 2 equals signs compare if something is the same
-print("ian") == (" Ian") # same as 
