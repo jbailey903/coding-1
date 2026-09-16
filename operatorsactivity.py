ff5f0444ddab39496fae4133384cf67214186a13
jbailey903 <jbailey903@boyslatin.net>
2026-09-16 13:42:21 +0000

    opperators

--- operatorsactivity.py
+++ operatorsactivity.py
@@ -5,4 +5,20 @@ print(18)
 print( " coding 1" " Coding 1")
 # false
 print("0" 0)
-# false 
+# false
+
+book = 10.99
+tablet = 399.900
+
+print( Book + Tablet)
+
+cart=98.87
+amountforDiscount
+
+print( Cart > Discount)
+
+shoes=200.00
+Tax= 0.007
+
+GPA = 86
+print(GPA > 85 and recomidition == True)
